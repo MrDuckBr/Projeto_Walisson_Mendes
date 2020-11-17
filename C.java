c86da1c53bc019c73f0a55037a7429fbd4417fdb
MrDuckBr <wwalisson2012@gmail.com>
2020-11-17 19:38:58 -0300

    Criação da Classe C

---
+++ C.java
@@ -0,0 +1,15 @@
+
+public class C {
+	
+	String C1;
+	int C2;
+	
+	public void MC1() {
+		System.out.println("Método MC1");
+	}
+	
+	public void MC2() {
+		System.out.println("Método MC2");
+	}
+
+}
